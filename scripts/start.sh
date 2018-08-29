2dceca36f7a80cc187a1e36cf49272f3f4d712d2
Vladimir Tsykun <vtsykun@okvpn.org>
2018-08-30 01:32:51 +0300

    Update docker config

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,39 +1,12 @@
 #!/bin/bash
 
-# Reset
-Color_Off='\033[0m'       # Text Reset
-
-# Regular Colors
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
-
 touch lock.txt
-
-echo -e "$Yellow"
-echo "======================================================================"
-echo "PRIVATE_REPO_DOMAIN env var is now PRIVATE_REPO_DOMAIN_LIST !!!       "
-echo "======================================================================"
-echo -e "$Color_Off"
-#exit 0
-
-if [ ! -z "$PRIVATE_REPO_DOMAIN" ]; then
-  echo ""
-  echo -e "$Yellow"
-  echo "======================================================================"
-  echo "PRIVATE_REPO_DOMAIN env var is now PRIVATE_REPO_DOMAIN_LIST !!!       "
-  echo "======================================================================"
-  echo -e "$Color_Off"
-  exit 1
-fi
-
-mkdir -p /var/www/.ssh/
-mkdir -p /root/.ssh/
+mkdir -p /var/www/.ssh/ && mkdir -p /root/.ssh/
 
 if [ -d /var/tmp/ssh ]; then
     ls -l /var/tmp/ssh
     echo " >> Copying host ssh config from /var/tmp/ssh to /root/.ssh"
-    cp -r /var/tmp/ssh/* /var/www/.ssh
+    cp -r /var/tmp/ssh/* /var/www/.ssh/
     chmod -R 600 /var/www/.ssh/*
 fi
 
@@ -49,11 +22,7 @@ for _DOMAIN in $PRIVATE_REPO_DOMAIN_LIST ; do
     fi
 done
 
-cp -r /var/www/.ssh/* /root/.ssh
-chmod -R 600 /root/.ssh/*
-
-echo " >> Building Packagest for the first time"
-
+cp -r /var/www/.ssh/* /root/.ssh && chmod -R 600 /root/.ssh/*
 FILE=app/config/parameters.yml
 if [ -f ${FILE} ]; then
     sed -i "s/database_host"\:".*/database_host"\:" $DATABASE_HOST/g" ${FILE}
@@ -65,7 +34,11 @@ if [ -f ${FILE} ]; then
     sed -i "s/packagist_dist_host"\:".*/packagist_dist_host"\:" https:\/\/$VIRTUAL_HOST/g" ${FILE}
 fi
 
-composer install --no-interaction --no-suggest --prefer-dist
+# Additional script handler
+if [ -f /var/tmp/data/handler.sh ]; then
+    bash /var/tmp/data/handler.sh
+fi
+
 app doctrine:schema:update --force -v
 DB_DRIVER=`cat ${FILE} | awk '/database_driver:/{ print $2 }'`
 case "$DB_DRIVER" in
@@ -74,8 +47,8 @@ case "$DB_DRIVER" in
     ;;
 esac
 
-rm -r var/cache/* var/logs/*
-chown www-data:www-data -R /var/www/
+rm -rf var/cache/* var/logs/*
+app cache:clear --env=prod && app cache:clear --env=dev && \
+    chown www-data:www-data -R /var/www/ && rm lock.txt
 
-rm lock.txt
 exit 0
